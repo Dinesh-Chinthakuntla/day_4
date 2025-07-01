ce7782d493169cbc62b0fbb992c9b9d66656109c
Dinesh Chinthakuntla <dineshchinthakuntla@gmail.com>
2025-07-01 15:32:40 +0530

    slicing and list (homogeneous)

---
+++ list_homogeneous.py
@@ -0,0 +1,4 @@
+#list homogeneous elements
+a=[1,2,3,4,5]
+print(a)
+print(type(a))
---
+++ slicing_backward.py
@@ -0,0 +1,10 @@
+#slicing from backward direction
+a="OVERDUE LOAN PAYMENT"
+b=a[-1:-8:-1]
+c=a[-7: :1]
+d=a[-12:-8:1]
+e=a[11:7:-1]
+print(b)
+print(c)
+print(d)
+print(e)
